953779c38afae310cad43cf265c0bab03544121d
root <raemond.arellano01@gmail.com>
2025-07-03 17:09:20 +0800

    add if statement and sub

---
+++ 03-if_statement/02-script.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+count=10
+
+if [ $count -eq 9 ]
+then
+	echo "condition is 9"
+elif [ $count -eq 10 ]
+then
+	echo "condition is true 10"
+else
+	echo "condition is false"
+fi
---
+++ 04-command_substitution/script.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "command substituion"
+
+
+current_directory=$(pwd)
+
+
+echo "your present working dir is: " $current_directory
+
+newfile= cat>hello.txt
+
+echo $newfile

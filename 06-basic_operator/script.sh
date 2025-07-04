06b848cceb4ca70ae9d5d3690f5c2a4839aa519e
root <raemond.arellano01@gmail.com>
2025-07-04 17:45:24 +0800

    basic operator

---
+++ 06-basic_operator/script.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+
+read -p "Enter n1: " n1
+read -p "Enter n2: " n2
+
+echo "ADDITION" $((n1+n2))
+
+echo "SUBTRACTION" $((n1-n2))
+
+echo "DIVISION" $((n1/n2))
+
+echo "MULTIPLY" $((n1*n2))
+

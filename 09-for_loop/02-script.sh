2234226107a3124d6ce58e0819c1cc4f146906fc
root <raemond.arellano01@gmail.com>
2025-07-08 16:57:41 +0800

    for loop

---
+++ 09-for_loop/02-script.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+myarray=(python c++ c java js shellscripting)
+
+for ((i=0; i<5; i++)){
+    echo ${myarray[i]}
+}
---
+++ 09-for_loop/03-script.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+
+for i in mond
+do
+  if [ $i == mond ]
+  then
+  	 echo "mond is a good boy"
+   else
+	 echo "mond is not good boy"	
+  fi
+done
